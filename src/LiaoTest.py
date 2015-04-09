52f31235c9b5d1db5bf7efb30c5b278f8ce2ebea
northyoung <joker.ssy@gmail.com>
2015-04-09 19:32:54 +0800

    python basic

---
+++ src/LiaoTest.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+__author__ = 'sunsy3'
+
+#print print '100 + 200 =', 100 + 200
+# name = raw_input('please input your name:')
+# print name
+# ord('65')
+# chr(65)
+# print u'故宫'
+# print u'ABC'.encode('utf-8')
+# print 'abc'.decode('utf-8')
+###list
+classmates = ['Michael', 'Bob', 'Tracy']
+classmates.append('Young')
+print len(classmates)
+classmates2 = ('Michael', 'Bob', 'Tracy','Blami','Young')
+#进行切片操作
+print classmates2[0:3]
+print classmates2[1:3]
+List = range(0,100)
+print List[:10:2]
+
+
