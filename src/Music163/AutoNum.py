101e6224cac9a3cb5395b439b4484c71066575d0
northyoung <joker.ssy@gmail.com>
2015-04-15 15:18:31 +0800

    #获取mp3文件的ID3信息 def getid3data(file):

--- src/Music163/AutoNum.py
+++ src/Music163/AutoNum.py
@@ -3,8 +3,27 @@
 
 import os
 import urllib2
+import binascii
+import json
+
+id3_data = {}
+frame_id = ['TIT2', 'TYER', 'TRCK', 'TALB','TPE2' ,'COMM','TPE1']
+
+#获取mp3文件的ID3信息
+def getid3data(file):
+    file.read(10)
+    while(True):
+        id3_head = file.read(4)
+        if id3_head not in frame_id:
+            break
+        size = int(binascii.b2a_hex(file.read(4)),16)
+        file.read(2) #标志位
+        id3_data[id3_head] = file.read(size).strip('\x00')
+    return id3_data
 
 
 if __name__ == '__main__':
-	file = open('D:\CloudMusic\The Chemical Brothers - Believe.mp3')
-	
+    file = open('D:\CloudMusic\The Chemical Brothers - Believe.mp3','rb')
+    print getid3data(file)
+    # print json.dumps(jsonResult, ensure_ascii=False, encoding='gbk')
+
