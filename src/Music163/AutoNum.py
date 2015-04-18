ebaf2324faed94d36e1b4244bfff28c77ef2aaa6
northyoung <joker.ssy@gmail.com>
2015-04-19 02:39:20 +0800

    成功抓取专辑歌曲名信息，准备组合歌曲名和艺术家信息。进行本地音轨修改

--- src/Music163/AutoNum.py
+++ src/Music163/AutoNum.py
@@ -58,21 +58,66 @@ def modCdInfo(filename,num,sum):
     # audiofile.tag.recording_date = 1990
     audiofile.tag.save()
 
-def getMusicInfo(url):
-    header = {
-        'Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-        'Accept-Language','zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3'
-        'Content-Type','application/x-www-form-urlencoded'
-        'Referer','http://music.163.com/'
-        'User-Agent','Mozilla/5.0 (Windows NT 6.1; rv:34.0) Gecko/20100101 Firefox/34.0'
-        'Proxy-Connection','keep-alive'
-        'Cookie','usertrack=ezq0aVTdvJ4eTEC8RZWyAg==; NETEASE_WDA_UID=33293408#|#1408087419403; usertrack=ZUcIhVT1I8u/zRQJIKN7Ag==; _ntes_nnid=9d33ac46010a49c7f6fc33a33cfed3c2,1425351633975; _ntes_nuid=9d33ac46010a49c7f6fc33a33cfed3c2; vjuids=38458f2ea.14be32ef121.0.19dea6d1; vjlast=1425445679.1425618427.13; vinfo_n_f_l_n3=1daee5b686cfee08.1.1.1425445679429.1425445685632.1425618462681; JSESSIONID-WYYY=ec99c6d12da1ba3d4590024991f7a07e675df2bca6947da396a64ab53e940b9e7e7776a196db9706f05ecb86aabeb3df8e3b4c6147aad7ea497c0448d8da83100999fb9c6c5ca719214a9a0669a55d5ababf89e1263573bd00fb58ce7829712330c0fc29c422959fe7a0cdb15e12dd13168a81903c021274d3f1880bc02267dd92811180; __utma=94650624.1922532604.1423817897.1426595771.1429185751.6; __utmb=94650624.8.10.1429185751; __utmc=94650624; __utmz=94650624.1426158782.3.3.utmcsr=baidu|utmccn=(organic)|utmcmd=organic|utmctr=%E7%BD%91%E6%98%93%E4%BA%91%E9%9F%B3%E4%B9%90'
+#向服务器发送获取专辑信息的请求
+#url:请求的专辑url地址
+def requestMusicInfo(url):
+    headers = {
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language':'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',
+        'Content-Type':'application/x-www-form-urlencoded',
+        'Referer':'http://music.163.com/',
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; rv:34.0) Gecko/20100101 Firefox/34.0',
+        'Proxy-Connection':'keep-alive',
+        'Cookie':'usertrack=ezq0aVTdvJ4eTEC8RZWyAg==; NETEASE_WDA_UID=33293408#|#1408087419403; usertrack=ZUcIhVT1I8u/zRQJIKN7Ag==; _ntes_nnid=9d33ac46010a49c7f6fc33a33cfed3c2,1425351633975; _ntes_nuid=9d33ac46010a49c7f6fc33a33cfed3c2; vjuids=38458f2ea.14be32ef121.0.19dea6d1; vjlast=1425445679.1425618427.13; vinfo_n_f_l_n3=1daee5b686cfee08.1.1.1425445679429.1425445685632.1425618462681; JSESSIONID-WYYY=ec99c6d12da1ba3d4590024991f7a07e675df2bca6947da396a64ab53e940b9e7e7776a196db9706f05ecb86aabeb3df8e3b4c6147aad7ea497c0448d8da83100999fb9c6c5ca719214a9a0669a55d5ababf89e1263573bd00fb58ce7829712330c0fc29c422959fe7a0cdb15e12dd13168a81903c021274d3f1880bc02267dd92811180; __utma=94650624.1922532604.1423817897.1426595771.1429185751.6; __utmb=94650624.8.10.1429185751; __utmc=94650624; __utmz=94650624.1426158782.3.3.utmcsr=baidu|utmccn=(organic)|utmcmd=organic|utmctr=%E7%BD%91%E6%98%93%E4%BA%91%E9%9F%B3%E4%B9%90'
     }
-    response = urllib2.urlopen(url,header)
-    return response.read()
+    req = urllib2.Request(url)
+    req.add_header('Accept',headers['Accept'])
+    req.add_header('Accept-Language',headers['Accept-Language'])
+    req.add_header('Content-Type',headers['Content-Type'])
+    req.add_header('Referer',headers['Referer'])
+    req.add_header('User-Agent',headers['User-Agent'])
+    req.add_header('Proxy-Connection',headers['Proxy-Connection'])
+    req.add_header('Cookie',headers['Cookie'])
+    response = urllib2.urlopen(req)
+    html = response.read()
+    # print html
+    return html
+
+#解析服务器返回的数据
+#url:请求的专辑url地址
+def getMusicInfo(url):
+    outList = []
+    soup = BeautifulSoup(requestMusicInfo(url))
+    CdInfo = soup.find("h2","f-ff2").get_text()
+    print u'专辑名称:',CdInfo
+    # songListHtml = soup.find(id="m-song-list-module") #歌曲列表
+    songList = soup.find("div","f-cb").findAll("span","txt")
+    artistList = soup.findAll("div","text")
+    for link in artistList:
+        link
+    print artistList
+    for i in range(len(songList)):
+        songName = BeautifulSoup(str(songList[i])).get_text().strip()#获取歌曲名称
+        artistName = BeautifulSoup(str(artistList[i])).get_text().strip()#获取歌曲艺术家
+        print u'第',str(i+1),u'首:',artistName," - ",songName
+        outList.append(" - "+songName.encode('gbk'))
+    return outList
+
+#查找本地文件夹下面的音乐文件，进行匹配和名称修改
+#dirName 音乐文件的下载地址
+#MusicList 专辑歌曲列表 从网络获得
+def getLocalFile(dirName,MusicList):
+    print MusicList
+    dirNameList = os.listdir(dirName)
+    print dirNameList
+    for count in range(len(MusicList)):
+        for fileName in dirNameList:
+            if MusicList[count].decode('utf-8') is fileName:
+                print count,u' is ',fileName
 
 if __name__ == '__main__':
+    kid_a = 'http://music.163.com/album?id=2065424' #KID A -- radiohead
 
-    soup = BeautifulSoup(response.read())
-    songList = soup.find(id="m-song-list-module")
-    print songList
+    dirName = 'F:\CloudMusic' #文件路径
+    fileList = getMusicInfo(kid_a)
+    getLocalFile(dirName,fileList)
